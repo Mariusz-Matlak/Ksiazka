272d91596191a03f8bdb2fa128b44f81a845d1a2
mtm915 <1mariusz.matlak@gmail.com>
2019-02-10 13:42:34 +0100

    first commit

---
+++ module/Ksiegarnia/src/Controller/IndexController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Ksiegarnia\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+    public function editAction()
+    {
+
+    }
+
+    public function addAction()
+    {
+
+    }
+
+    public function deleteAction()
+    {
+
+    }
+
+    public function saveAction()
+    {
+
+    }
+}
